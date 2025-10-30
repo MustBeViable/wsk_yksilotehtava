1b397e65975495c433b63ad4343736798b674b33
MustBeViable <elias1rinne@gmail.com>
2025-10-30 11:58:47 +0200

    add visualisation for errors for user

---
+++ components/error_component.js
@@ -0,0 +1,34 @@
+export const failedToLoad = (element, message, buttonText ) => {
+  const errorElement = document.createElement(element);
+  const div = document.getElementById('error');
+  document.querySelector("body").appendChild(div);
+  div.innerHTML = "";
+  div.setAttribute("role", "alert");
+  div.setAttribute("aria-live", "polite")
+  div.appendChild(errorElement);
+  switch (element) {
+    case "dialog": {
+      errorElement.innerHTML = `
+      <p><b>${message}</p></b>
+      <button id="close_me">${buttonText}</button>
+      `;
+      document.getElementById("close_me")?.addEventListener("click", (e) => {
+        e.preventDefault();
+        errorElement.close();
+        div.innerHTML = "";
+      });
+      errorElement.showModal();
+      break;
+    }
+    case "div": {
+      errorElement.innerHTML = `
+      <p><b>${message}</p></b>
+      `;
+    }
+    case "p": {
+      errorElement.innerHTML = `
+      <p><b>${message}</p></b>
+      `
+    }
+  }
+};
